731d80502abdd610fc4e71bc71b71d540a822e41
afonsorc <afonsorc@outlook.com>
2024-12-06 20:25:44 +0000

    feat: add template setup

---
+++ base/src/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace base{
+    class Program {
+        static void Main(string[] args){
+
+            // read and process input onto array
+            string[] input = System.IO.File.ReadAllLines("../input/input.txt");
+
+            // write solutions
+            System.Console.WriteLine("----------------\nPart1: {0}\n----------------", partOne(input));
+            System.Console.WriteLine("----------------\nPart2: {0}\n----------------", partTwo(input));
+            return;
+        }
+
+
+        static int partOne(string[] input){
+            
+            int solution = 0;
+
+            
+
+            return solution;
+        }
+
+
+        static int partTwo(string[] input){
+            
+            int solution = 0;
+
+
+
+            return solution;
+        }
+    }
+}
